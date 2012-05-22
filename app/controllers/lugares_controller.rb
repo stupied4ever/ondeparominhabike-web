ca1417a861b2d0dd632af33323ccae3242062b41
Rafael Almeida <ralmeida@parsecdigital.com.br>
2012-05-22 14:13:05 -0300

    inclui um metodo para contar os bicicletarios existentes para atualizacao

--- app/controllers/lugares_controller.rb
+++ app/controllers/lugares_controller.rb
@@ -5,9 +5,21 @@ class LugaresController < InheritedResources::Base
         format.html 
         format.json { 
           ultimo_atualizado = @lugares.ultimo_atualizado.first 
-          @sincronizacao = {:ultima_atualizacao => (ultimo_atualizado ? ultimo_atualizado.updated_at + 1.second : nil) , :lugares => @lugares}
+          @sincronizacao = {:ultima_atualizacao => (ultimo_atualizado ? ultimo_atualizado.updated_at + 1.second : nil) , :lugares => @lugares, :quantidade => @lugares.size}
           render json: @sincronizacao
         }
       end
    end
+   
+   def count
+       @lugares = Lugar.desde(params[:desde]) if params[:desde]
+       index! do |format|
+         format.html 
+         format.json { 
+           ultimo_atualizado = @lugares.ultimo_atualizado.first 
+           @sincronizacao = {:ultima_atualizacao => (ultimo_atualizado ? ultimo_atualizado.updated_at + 1.second : nil) , :quantidade => @lugares.size }
+           render json: @sincronizacao
+         }
+       end
+    end
 end
